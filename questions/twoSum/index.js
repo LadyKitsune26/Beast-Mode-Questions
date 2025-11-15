7723993ce90fadb838b6e28ecae5f47bfce7bf1e
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-14 19:26:24 -0600

    Two Sum Done

--- questions/twoSum/index.js
+++ questions/twoSum/index.js
@@ -10,8 +10,48 @@
  * 
  */
 
+
+// // junior way, inefficient, looping over 25 times. $60K - $80K
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length - 1; ++i){
+//         for (let j = 0; j< nums.length; ++j){
+//             if (nums[i] + nums[j]   === target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+
+
+// // Mid level solution $80K - $100K
+// // O(n^2) time | O(1) space
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length - 1; ++i){
+//         for (let j = i + 1; j < nums.length; ++j){
+//             if (nums[i] + nums[j] === target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+
+
+// Top tech solution
+// O(n) time 
 const twoSum = (nums, target) => {
+    const mapDifferenceToIndex = new Map();
+
+    for (let i = 0; i < nums.length; ++i) {
+        if (mapDifferenceToIndex.has(nums[i])){
+            return [mapDifferenceToIndex.get(nums[i]), i]
+        }
+        const difference = target - nums[i]
+        mapDifferenceToIndex.set(difference, i)
+    }
 }
+
+
+
 
 module.exports = twoSum;
 
