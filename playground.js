d0112e124b2f9be9e5bf5f6186d1893cbcf0c4bd
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-14 20:03:24 -0600

    Duplicate complete

--- playground.js
+++ playground.js
@@ -1,12 +1,12 @@
-const nums = [1, 2, 4, 9]
-const target = 13
+// const nums = [1, 2, 4, 9]
+// const target = 13
 
-const map = new Map();
+// const map = new Map();
 
-for (let i = 0; i < nums.length; ++i) {
-  map.set(nums[i], true)
+// for (let i = 0; i < nums.length; ++i) {
+//   map.set(nums[i], true)
 
-  if (nums[i] === 9) {
-    console.log(map.get(4)) // O(1) time
-  }
-}
+//   if (nums[i] === 9) {
+//     console.log(map.get(4)) // O(1) time
+//   }
+// }
--- questions/containsDuplicate/index.js
+++ questions/containsDuplicate/index.js
@@ -10,8 +10,85 @@
  * 
  */
 
-const containsDuplicate = (nums) => {
 
+// // 1st Solution $60K - $80K
+// // O(n^2) time | O(n) space
+// const containsDuplicate = (nums) => {
+//  for (let i = 0; i < nums.length - 1; ++i){
+//     for (let j = i + 1; j < nums.length; ++j) {
+//         if (nums[i] === nums[j]) {
+//             return true
+//         }
+//     }
+//  }
+//  return false
+// }
+
+// module.exports = containsDuplicate;
+
+
+// // Sorting solution $80K - $100K 
+// // O(nlog(n)) time | O(n) space
+// const containsDuplicate = (nums) => {
+//     const sortedArr = nums.sort((a, b) => a - b) // O(n) space | O(nlog(n)) time
+
+//     for (let i = 0; i < sortedArr.length - 1; ++i) { // O(n) time
+//         if (sortedArr[i]  === sortedArr[i + 1]) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+// module.exports = containsDuplicate;
+
+
+// Optimal solution 
+const containsDuplicate = (nums) => {
+    const map = new Map()
+
+    for (let i = 0; i < nums.length; ++i){
+        if(map.has(nums[i])){
+            return true
+        }
+        map.set(nums[i], true)
+    }
+    return false; 
 }
 
 module.exports = containsDuplicate;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Chat GPT Solution #1
+// const containsDuplicate = (nums) => {
+//   const set = new Set(nums);
+//   return set.size !== nums.length;
+// }
+
+// module.exports = containsDuplicate;
+
+
+// // Chat GPT Solution #2 - using a loop + set(more explicit)
+// const containsDuplicate = (nums) => {
+//   const seen = new Set();
+
+//   for (let num of nums) {
+//     if (seen.has(num)) return true;
+//     seen.add(num);
+//   }
+
+//   return false;
+// }
+
+// module.exports = containsDuplicate;
