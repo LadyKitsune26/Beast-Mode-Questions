a06d137fb2ac5ddaf725733aeab9ace7424e96a0
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-16 17:17:33 -0600

    Stocks complete

--- questions/stocks/index.js
+++ questions/stocks/index.js
@@ -10,10 +10,89 @@
  * stocks([2, 9, 18, 9, 2]) ->  16
  */
 
+
+
+
+
+// // David's SOlution #1
+// const stocks = (prices) => {
+//   let l = 0;
+//   let r = 1;
+//   maxProfit = 0;
+
+//   for (let r = 1; r < prices.length; ++r) {
+//     const profit = prices[r] - prices[l];
+//     if (profit > 0) {
+//       maxProfit = Math.max(maxProfit, profit);
+//     } else {
+//       l = r;
+//     }
+//   }
+
+//   return maxProfit;
+// };
+
+// module.exports = stocks;
+
+
+
+// David's Solution #2
 const stocks = (prices) => {
-   
-}
+  let l = 0;
+  let r = 1;
+  maxProfit = 0;
 
+  while (r < prices.length) {
+    const profit = prices[r] - prices[l]
+    if (profit > 0) {
+      maxProfit = Math.max(maxProfit, profit);
+    } 
+    else {
+      l = r;
+    }
+    ++r;
+  }
 
-//DO NOT EDIT BELOW THIS LINE
+  return maxProfit;
+};
+
 module.exports = stocks;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Chat GPT Solution
+// const stocks = (prices) => {
+//   let minPrice = prices[0];
+//   let maxProfit = 0;
+
+//   for (let i = 1; i < prices.length; i++) {
+//     const price = prices[i];
+
+//     // Check potential profit if selling today
+//     const profit = price - minPrice;
+//     if (profit > maxProfit) {
+//       maxProfit = profit;
+//     }
+
+//     // Update the minimum price seen so far
+//     if (price < minPrice) {
+//       minPrice = price;
+//     }
+//   }
+
+//   return maxProfit;
+// };
+
+// module.exports = stocks;
