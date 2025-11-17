39ac6e09d7ba97eddae097f7adf03f617a934aa1
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-16 21:03:51 -0600

    finally fucjing finished with this section never toiuching it again

--- questions/searchRotatedArray/index.js
+++ questions/searchRotatedArray/index.js
@@ -14,7 +14,81 @@
  */
 
 const search = (nums, target) => {
+    
+    let l = 0
+    let r = nums.length - 1
+
+    while (l <= r){
+        let mid = Math.floor((l + r) / 2)
+
+        if (nums[mid] === target) return mid
+
+        //In the green portion of the array
+
+        if (nums[mid] < nums[r]){
+            if (target < nums[mid] || target > nums[r]){
+                r = mid - 1
+            }
+            else{
+                l = mid + 1
+            }
+        }
+        //In the blue portion of the array
+        else {
+            if (target > nums[mid] || target < nums[l]){
+                l = mid + 1
+            }
+            else r = mid - 1
+        }
+
+    }
+
+    return "not found"
 
 }
 
 module.exports = search;
+
+
+
+
+
+
+
+
+
+// // Chat GPT Solution
+// const search = (nums, target) => {
+//   let left = 0;
+//   let right = nums.length - 1;
+
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
+
+//     if (nums[mid] === target) return mid;
+
+//     // Check if left half is sorted
+//     if (nums[left] <= nums[mid]) {
+//       // Target is within the left sorted range
+//       if (nums[left] <= target && target < nums[mid]) {
+//         right = mid - 1;
+//       } else {
+//         left = mid + 1;
+//       }
+//     } 
+    
+//     // Otherwise, right half is sorted
+//     else {
+//       // Target is within the right sorted range
+//       if (nums[mid] < target && target <= nums[right]) {
+//         left = mid + 1;
+//       } else {
+//         right = mid - 1;
+//       }
+//     }
+//   }
+
+//   return -1; // Target not found
+// };
+
+// module.exports = search;
