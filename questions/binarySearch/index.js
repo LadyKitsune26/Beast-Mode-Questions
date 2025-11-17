6ca4d710cfdfe7b4cc99b9e7e47baf5d9a685546
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-16 18:28:42 -0600

    finished binary Search

--- questions/binarySearch/index.js
+++ questions/binarySearch/index.js
@@ -12,9 +12,65 @@
  * 
  */
 
+
+
+// Frontend Simplified Solution
 const binarySearch = (nums, target) => {
+  let l = 0;
+  let r = nums.length;
 
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
 
-}
+    if (nums[mid] > target) {
+      r = mid - 1;
+    } 
+    else if (nums[mid] < target) {
+      l = mid + 1;
+    } 
+    else {
+      return mid;
+    }
+  }
+};
 
 module.exports = binarySearch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Chat GPT Solution
+// const binarySearch = (nums, target) => {
+//   let left = 0;
+//   let right = nums.length - 1;
+
+//   while (left <= right) {
+//     let mid = Math.floor((left + right) / 2);
+
+//     if (nums[mid] === target) {
+//       return mid;
+//     }
+
+//     if (nums[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   // Not found
+//   return -1;
+// };
+
+// module.exports = binarySearch;
