1dfd0a459d7b3ec4bf7148345501b17cf62d0424
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-16 20:00:49 -0600

    finished max subarray

--- questions/maxSubarray/index.js
+++ questions/maxSubarray/index.js
@@ -9,9 +9,82 @@
   * maxSubArray([2, 4, 3, -1, -4, -9]) = 9
   */
 
-const maxSubArray = (nums) => {
 
+// // David's solution #1 O(n^3)
+// const maxSubArray = (nums) => {
+//   let maxSum = nums[0]
+
+
+//   for (let i = 0; i < nums.length; ++i) { // O(n) * O (n^2) -> O(n^3)
+//     for (let j = i + 1; j <= nums.length; ++j){ // O(n) * O(n) -> O(n^2)
+//       const subarray = nums.slice(i, j)
+//       const sum = subarray.reduce((acc, curr) => acc + curr)
+//       maxSum = Math.max(maxSum, sum)
+//     }
+//   }
+//   return maxSum
+// }
+
+// module.exports = maxSubArray;
+
+
+
+// // David's Solution #2 // O(n^2) time | O(1) space
+// const maxSubArray = (nums) => {
+//   let maxSum = nums[0]
+
+//   for (let i = 0; i < nums.length; ++i) {
+//     let sum = 0;
+//     for (let j = i; j < nums.length; ++j) {
+//       sum += nums[j]
+//       maxSum = Math.max(maxSum, sum);
+//     }
+//   }
+//   return maxSum
+// }
+
+// module.exports = maxSubArray;
+
+
+
+
+// David's Solution #3 most Optimal solution
+const maxSubArray = (nums) => {
+  let maxSum = nums[0]
+  let sum = 0
+
+  for (let i = 0; i < nums.length; ++i){
+    if (sum < 0) {
+      sum = 0
+    }
+    sum += nums[i]
+    if (maxSum < sum){
+      maxSum = sum
+    }
+  }
+  return maxSum
 }
 
-//DO NOT EDIT BELOW THIS LINE
 module.exports = maxSubArray;
+
+
+
+
+
+
+
+// // Chat GPT Solution
+// const maxSubArray = (nums) => {
+//   let currentSum = nums[0];
+//   let maxSum = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     // either extend the current subarray or start a new one
+//     currentSum = Math.max(nums[i], currentSum + nums[i]);
+//     maxSum = Math.max(maxSum, currentSum);
+//   }
+
+//   return maxSum;
+// };
+
+// module.exports = maxSubArray;
