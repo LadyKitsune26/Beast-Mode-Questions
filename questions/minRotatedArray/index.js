05322013618a635922630cfeb0c6bb83bfa836ab
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-16 19:15:09 -0600

    finished Min Rotated Array

--- questions/minRotatedArray/index.js
+++ questions/minRotatedArray/index.js
@@ -13,7 +13,54 @@
  */
 
 const findMinimum = (nums) => {
+    let l = 0;
+    let r = nums.length - 1;
 
+
+    while ( l < r) {
+        let mid = Math.floor((l + r) / 2)
+
+        if (nums[mid] > nums[r]) {
+            l = mid + 1
+        }
+        else {
+            r = mid
+        }
+    }
+    return nums[l]
 }
 
 module.exports = findMinimum;
+
+
+
+
+
+
+
+
+
+
+
+// // Chat GPT Solution
+// const findMinimum = (nums) => {
+//   let left = 0;
+//   let right = nums.length - 1;
+
+//   while (left < right) {
+//     let mid = Math.floor((left + right) / 2);
+
+//     // If mid value is greater than right value,
+//     // min must be in the right half
+//     if (nums[mid] > nums[right]) {
+//       left = mid + 1;
+//     } else {
+//       // Otherwise, min is at mid or left half
+//       right = mid;
+//     }
+//   }
+
+//   return nums[left];
+// };
+
+// module.exports = findMinimum;
